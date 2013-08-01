fc27c5f123bb0daf735040e83b2b2261ba17486b
Ilya Scharrenbroich <$EMAIL>
2013-08-01 12:34:59 -0400

    adding hstore to db

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -105,7 +105,7 @@ if ['solo','db_master', 'db_slave'].include?(node[:instance_role])
   # postgresql9_dict_xsyn "dbname"
   # postgresql9_earthdistance "dbname"
   # postgresql9_fuzzystrmatch "dbname"
-  # postgresql9_hstore "dbname"
+  postgresql9_hstore 'mommy_nearest'
   # postgresql9_intarray "dbname"
   # postgresql9_isn "dbname"
   # postgresql9_lo "dbname"
